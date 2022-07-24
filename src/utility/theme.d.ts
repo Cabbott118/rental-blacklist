72957d8238181f05ac1e17b1a6472a4cb99dae29
CalebAbbott <caleb.abbott@capco.com>
2022-07-24 17:17:15 -0400

    React Typescript template

---
+++ src/utility/theme.d.ts
@@ -0,0 +1,20 @@
+// import { Theme, ThemeOptions } from '@mui/material/styles';
+
+// declare module '@mui/material/styles' {
+//   interface CustomTheme extends Theme {
+//     palette: {
+//       primary: {
+//         main: string;
+//       };
+//     };
+//   }
+//   // allow configuration using `createTheme`
+//   interface CustomThemeOptions extends ThemeOptions {
+//     palette?: {
+//       primary?: {
+//         main?: string;
+//       };
+//     };
+//   }
+//   export function createTheme(options?: CustomThemeOptions): CustomTheme;
+// }
